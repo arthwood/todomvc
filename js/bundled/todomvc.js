ab31a314496a8c43bc42b1487649340c0198623c
arthwood <admin@arturbilski.pl>
2015-06-09 22:09:15 +0100

    Use non-minified version of todomvc.

--- js/bundled/todomvc.js
+++ js/bundled/todomvc.js
@@ -1,1 +1,368 @@
-artjs.TemplateLibrary.config.BASE_TEMPLATES=[],artjs.TemplateLibrary.config.PATH="templates",artjs.TemplateLibrary.config.TEMPLATES.push("item","filter"),window.art={controller:{},model:{},service:{},view:{}},art.controller.Main=artjs.Class(null,{index:function(a){artjs.Broadcaster.fire("Filter",a.controllerId)},toString:function(){return this.ctor.toString()}},{_name:"art.controller.Main",toString:function(){return this._name}},artjs.Controller),artjs.Router.defaultController=new art.controller.Main,art.model.Filter=artjs.Class(function(a,b){this.super(),this.addProperties({path:a,title:b})},null,{_name:"art.model.Filter",toString:function(){return this._name}},artjs.Model),art.model.Item=artjs.Class(function(a,b){this.super(),this.addProperties({value:a,completed:Boolean(b),visible:!0})},{toJson:function(){return{title:this.value,completed:this.completed}}},{_name:"art.model.Item",fromJson:function(a){return new this(a.title,Boolean(a.completed))},fromArray:function(a){return artjs.Array.map(a,this.fromJson,this)},toString:function(){return this._name}},artjs.Model),art.service.db={items:[{title:"Taste JavaScript"},{title:"Buy a unicorn"}]},art.service.ListWatcher=artjs.Class(function(a,b){this._listId=a,this._updateable=b,artjs.Component.onLoad(this._listId,artjs.$D(this,"_onListLoad"))},{toString:function(){return this.ctor.toString+" - listId: "+this._listId+", updateable: "+this._updateable.toString()},_onListLoad:function(a){var b=artjs.$D(this,"_onUpdate");this._listModel=a.getModel(),this._listModel.onItemAdd.add(b),this._listModel.onItemRemove.add(b),this._listModel.onItemChange.add(b),this._listModel.addPropertyListener("items",b,!0)},_onUpdate:function(){this._updateable.update(this._listModel)}},{_name:"art.service.ListWatcher",toString:function(){return this._name}}),art.view.ClearCompleted=artjs.Class(function(a){this.super(a),this._listWatcher=new art.service.ListWatcher("todo-list",this)},{update:function(a){artjs.Element.setVisible(this._element,artjs.Array.any(artjs.Array.pluck(a.items,"completed")))}},{_name:"art.view.ClearCompleted"},artjs.Button),art.view.Complete=artjs.Class(function(a){this.super(a),this._handleBroadcast("Item::Complete","_onComplete")},{_onComplete:function(a){this.setChecked(a.getModel().completed)}},null,artjs.CheckBox),art.view.Count=artjs.Class(function(a){this.super(a);var b=new artjs.Model;b.addProperty("n"),this.setModel(b),this._listWatcher=new art.service.ListWatcher("todo-list",this)},{update:function(a){var b=a.items,c=b.length,d=artjs.Array.select(artjs.Array.pluck(b,"completed")).length;this._model.n=c-d}},{_name:"art.view.Count"},artjs.TemplateView),art.view.Edit=artjs.Class(function(a){this.super(a),artjs.on("blur",a,artjs.$D(this,"_onBlur")),artjs.on("keydown",a,artjs.$D(this,"_onEnter"),13),this._handleBroadcast("Item::Edit","_onEdit")},{_onEdit:function(a){this.setValue(a.getModel().value),this._element.focus()},_onBlur:function(){this._save()},_onEnter:function(){this._save()},_save:function(){this._fire("Item::Save")}},{_name:"art.view.Edit"},artjs.Text),art.view.Filter=artjs.Class(function(a){this.super(a),artjs.Broadcaster.addListener("Filter",artjs.$D(this,"_onFilter"))},{_onClick:function(a,b){this.super(a,b),this._toggle()},_onFilter:function(a){this.getHref()=="#/"+a&&this._toggle()},_toggle:function(){this.ctor.toggler.toggle(this._element)}},{_name:"art.view.Filter",toggler:new artjs.ClassToggler("selected")},artjs.Link),art.view.Filters=artjs.Class(function(a){this.super(a),this.setItems([new art.model.Filter("","All"),new art.model.Filter("active","Active"),new art.model.Filter("completed","Completed")])},null,{_name:"art.view.Filters"},artjs.ListView),art.view.Footer=artjs.Class(function(a){this.super(a),this._listWatcher=new art.service.ListWatcher("todo-list",this)},{update:function(a){artjs.Element.setVisible(this._element,artjs.Array.isNotEmpty(a.items))}},{_name:"art.view.Footer"},artjs.Component),art.view.Item=artjs.Class(function(a){this.super(a),this._handleEmit("Item::DblClick","_onEdit"),this._handleEmit("Item::Save","_onSave"),this._handleEmit("Item::Button::Delete","_onDelete"),this._handleEmit("Item::Complete","_onComplete"),this._editing=!1},{_onModelChange:function(){artjs.Element.setClass(this._element,this.ctor.COMPLETED_CLASS,this._model.completed),artjs.Element.setVisible(this._element,this._model.visible),this._fire("Item::Complete"),this._fire("Item::Save")},_onEdit:function(){this._editing||(this._editing=!0,artjs.Element.addClass(this._element,this.ctor.EDITING_CLASS),this._fire("Item::Edit"))},_onDelete:function(){this._fire("Item::Delete")},_onSave:function(a){this._editing=!1;var b=artjs.String.trim(a.getValue());artjs.Element.removeClass(this._element,this.ctor.EDITING_CLASS),artjs.String.isPresent(b)&&(this._model.value=b)},_onComplete:function(a){this._model.completed=a.isChecked()}},{EDITING_CLASS:"editing",COMPLETED_CLASS:"completed",_name:"art.view.Item"},artjs.TemplateView),art.view.ItemLabel=artjs.Class(function(a){this.super(a),artjs.on("dblclick",a,artjs.$D(this,"_onDblClick")),this._handleBroadcast("Item::Save","_onSave")},{_onDblClick:function(){this._fire("Item::DblClick")},_onSave:function(a){artjs.Element.setContent(this._element,a.getModel().value)}},{_name:"art.view.ItemLabel"},artjs.Component),art.view.List=artjs.Class(function(a){this.super(a),this._localStorage=new artjs.LocalStorage("todos-artjs"),this._listWatcher=new art.service.ListWatcher("todo-list",this),this._model.onItemAdd.add(artjs.$D(this,"_onItemAdd")),this._model.onItemChange.add(artjs.$D(this,"_onItemChange")),artjs.Broadcaster.addListener("Filter",artjs.$D(this,"_onFilter")),this._handle("Todo::New","_onNew"),this._handle("MarkAllComplete","_onMarkAllComplete"),this._handle("ClearCompleted","_onClearCompleted"),this._handleEmit("Item::Delete","_onDelete");var b=this._localStorage.getItem("items");this.setItems(art.model.Item.fromArray(!b||artjs.Array.isEmpty(b)?art.service.db.items:b))},{_onNew:function(a){var b=a.getValue();this.addItem(new art.model.Item(b))},_onDelete:function(a){this.removeItem(a.getModel())},update:function(){var a=this._model.items;artjs.Object.isNull(a)||(a=artjs.Array.invoke(a,"toJson")),this._localStorage.setItem("items",a)},_onMarkAllComplete:function(a){this._allComplete=a.isChecked(),artjs.Array.each(this._model.items,this._markAsComplete,this)},_onClearCompleted:function(){this.removeItems(artjs.Array.select(this._model.items,this._isCompleted,this))},_markAsComplete:function(a){a.completed=this._allComplete},_isCompleted:function(a){return a.completed},_isActive:function(a){return!a.completed},_true:function(){return!0},_onItemChange:function(a,b,c){c=="completed"&&this._setItemVisibility(b)},_onItemAdd:function(a,b){this._setItemVisibility(b)},_onFilter:function(a){this._visibilityStrategy=this.ctor.VISIBILITY_STRATEGIES[a]||"_true",this._filter()},_filter:function(){artjs.Array.each(this._model.items,this._setItemVisibility,this)},_setItemVisibility:function(a){a.visible=this[this._visibilityStrategy](a)}},{_name:"art.view.List",VISIBILITY_STRATEGIES:{completed:"_isCompleted",active:"_isActive"}},artjs.ListView),art.view.MarkAllComplete=artjs.Class(function(a){this.super(a),this._listWatcher=new art.service.ListWatcher("todo-list",this)},{update:function(a){var b=artjs.Array.pluck(a.items,"completed");this.setChecked(artjs.Array.all(b))}},{_name:"art.view.MarkAllComplete"},artjs.CheckBox),art.view.Main=artjs.Class(function(a){this.super(a),this._listWatcher=new art.service.ListWatcher("todo-list",this)},{update:function(a){artjs.Element.setVisible(this._element,artjs.Array.isNotEmpty(a.items))}},{_name:"art.view.Main"},artjs.View),art.view.New=artjs.Class(function(a){this.super(a),artjs.on("keydown",a,artjs.$D(this,"_onEnter"),13)},{_onEnter:function(){var a=artjs.String.trim(this.getValue());artjs.String.isEmpty(a)||(this._fire("Todo::New"),this.clear())}},{_name:"art.view.New"},artjs.Text)
+artjs.TemplateLibrary.config.BASE_TEMPLATES = [];
+
+artjs.TemplateLibrary.config.PATH = "templates";
+
+artjs.TemplateLibrary.config.TEMPLATES.push("item", "filter");
+
+window.art = {
+  controller: {},
+  model: {},
+  service: {},
+  view: {}
+};
+
+art.controller.Main = artjs.Class(null, {
+  index: function(request) {
+    artjs.Broadcaster.fire("Filter", request.controllerId);
+  },
+  toString: function() {
+    return this.ctor.toString();
+  }
+}, {
+  _name: "art.controller.Main",
+  toString: function() {
+    return this._name;
+  }
+}, artjs.Controller);
+
+artjs.Router.defaultController = new art.controller.Main;
+
+art.model.Filter = artjs.Class(function(path, title) {
+  this.super();
+  this.addProperties({
+    path: path,
+    title: title
+  });
+}, null, {
+  _name: "art.model.Filter",
+  toString: function() {
+    return this._name;
+  }
+}, artjs.Model);
+
+art.model.Item = artjs.Class(function(value, completed) {
+  this.super();
+  this.addProperties({
+    value: value,
+    completed: Boolean(completed),
+    visible: true
+  });
+}, {
+  toJson: function() {
+    return {
+      title: this.value,
+      completed: this.completed
+    };
+  }
+}, {
+  _name: "art.model.Item",
+  fromJson: function(data) {
+    return new this(data.title, Boolean(data.completed));
+  },
+  fromArray: function(data) {
+    return artjs.Array.map(data, this.fromJson, this);
+  },
+  toString: function() {
+    return this._name;
+  }
+}, artjs.Model);
+
+art.service.db = {
+  items: [ {
+    title: "Taste JavaScript"
+  }, {
+    title: "Buy a unicorn"
+  } ]
+};
+
+art.service.ListWatcher = artjs.Class(function(listId, updateable) {
+  this._listId = listId;
+  this._updateable = updateable;
+  artjs.Component.onLoad(this._listId, artjs.$D(this, "_onListLoad"));
+}, {
+  toString: function() {
+    return this.ctor.toString + " - listId: " + this._listId + ", updateable: " + this._updateable.toString();
+  },
+  _onListLoad: function(list) {
+    var onUpdateDelegate = artjs.$D(this, "_onUpdate");
+    this._listModel = list.getModel();
+    this._listModel.onItemAdd.add(onUpdateDelegate);
+    this._listModel.onItemRemove.add(onUpdateDelegate);
+    this._listModel.onItemChange.add(onUpdateDelegate);
+    this._listModel.addPropertyListener("items", onUpdateDelegate, true);
+  },
+  _onUpdate: function() {
+    this._updateable.update(this._listModel);
+  }
+}, {
+  _name: "art.service.ListWatcher",
+  toString: function() {
+    return this._name;
+  }
+});
+
+art.view.ClearCompleted = artjs.Class(function(element) {
+  this.super(element);
+  this._listWatcher = new art.service.ListWatcher("todo-list", this);
+}, {
+  update: function(listModel) {
+    artjs.Element.setVisible(this._element, artjs.Array.any(artjs.Array.pluck(listModel.items, "completed")));
+  }
+}, {
+  _name: "art.view.ClearCompleted"
+}, artjs.Button);
+
+art.view.Complete = artjs.Class(function(element) {
+  this.super(element);
+  this._handleBroadcast("Item::Complete", "_onComplete");
+}, {
+  _onComplete: function(item) {
+    this.setChecked(item.getModel().completed);
+  }
+}, null, artjs.CheckBox);
+
+art.view.Count = artjs.Class(function(element) {
+  this.super(element);
+  var model = new artjs.Model;
+  model.addProperty("n");
+  this.setModel(model);
+  this._listWatcher = new art.service.ListWatcher("todo-list", this);
+}, {
+  update: function(listModel) {
+    var items = listModel.items;
+    var total = items.length;
+    var completed = artjs.Array.select(artjs.Array.pluck(items, "completed")).length;
+    this._model.n = total - completed;
+  }
+}, {
+  _name: "art.view.Count"
+}, artjs.TemplateView);
+
+art.view.Edit = artjs.Class(function(element) {
+  this.super(element);
+  artjs.on("blur", element, artjs.$D(this, "_onBlur"));
+  artjs.on("keydown", element, artjs.$D(this, "_onEnter"), 13);
+  this._handleBroadcast("Item::Edit", "_onEdit");
+}, {
+  _onEdit: function(item) {
+    this.setValue(item.getModel().value);
+    this._element.focus();
+  },
+  _onBlur: function() {
+    this._save();
+  },
+  _onEnter: function() {
+    this._save();
+  },
+  _save: function() {
+    this._fire("Item::Save");
+  }
+}, {
+  _name: "art.view.Edit"
+}, artjs.Text);
+
+art.view.Filter = artjs.Class(function(element) {
+  this.super(element);
+  artjs.Broadcaster.addListener("Filter", artjs.$D(this, "_onFilter"));
+}, {
+  _onClick: function(e, ee) {
+    this.super(e, ee);
+    this._toggle();
+  },
+  _onFilter: function(id) {
+    if (this.getHref() == "#/" + id) {
+      this._toggle();
+    }
+  },
+  _toggle: function() {
+    this.ctor.toggler.toggle(this._element);
+  }
+}, {
+  _name: "art.view.Filter",
+  toggler: new artjs.ClassToggler("selected")
+}, artjs.Link);
+
+art.view.Filters = artjs.Class(function(element) {
+  this.super(element);
+  this.setItems([ new art.model.Filter("", "All"), new art.model.Filter("active", "Active"), new art.model.Filter("completed", "Completed") ]);
+}, null, {
+  _name: "art.view.Filters"
+}, artjs.ListView);
+
+art.view.Footer = artjs.Class(function(element) {
+  this.super(element);
+  this._listWatcher = new art.service.ListWatcher("todo-list", this);
+}, {
+  update: function(listModel) {
+    artjs.Element.setVisible(this._element, artjs.Array.isNotEmpty(listModel.items));
+  }
+}, {
+  _name: "art.view.Footer"
+}, artjs.Component);
+
+art.view.Item = artjs.Class(function(element) {
+  this.super(element);
+  this._handleEmit("Item::DblClick", "_onEdit");
+  this._handleEmit("Item::Save", "_onSave");
+  this._handleEmit("Item::Button::Delete", "_onDelete");
+  this._handleEmit("Item::Complete", "_onComplete");
+  this._editing = false;
+}, {
+  _onModelChange: function() {
+    artjs.Element.setClass(this._element, this.ctor.COMPLETED_CLASS, this._model.completed);
+    artjs.Element.setVisible(this._element, this._model.visible);
+    this._fire("Item::Complete");
+    this._fire("Item::Save");
+  },
+  _onEdit: function() {
+    if (!this._editing) {
+      this._editing = true;
+      artjs.Element.addClass(this._element, this.ctor.EDITING_CLASS);
+      this._fire("Item::Edit");
+    }
+  },
+  _onDelete: function() {
+    this._fire("Item::Delete");
+  },
+  _onSave: function(item) {
+    this._editing = false;
+    var value = artjs.String.trim(item.getValue());
+    artjs.Element.removeClass(this._element, this.ctor.EDITING_CLASS);
+    if (artjs.String.isPresent(value)) {
+      this._model.value = value;
+    }
+  },
+  _onComplete: function(checkbox) {
+    this._model.completed = checkbox.isChecked();
+  }
+}, {
+  EDITING_CLASS: "editing",
+  COMPLETED_CLASS: "completed",
+  _name: "art.view.Item"
+}, artjs.TemplateView);
+
+art.view.ItemLabel = artjs.Class(function(element) {
+  this.super(element);
+  artjs.on("dblclick", element, artjs.$D(this, "_onDblClick"));
+  this._handleBroadcast("Item::Save", "_onSave");
+}, {
+  _onDblClick: function() {
+    this._fire("Item::DblClick");
+  },
+  _onSave: function(item) {
+    artjs.Element.setContent(this._element, item.getModel().value);
+  }
+}, {
+  _name: "art.view.ItemLabel"
+}, artjs.Component);
+
+art.view.List = artjs.Class(function(element) {
+  this.super(element);
+  this._localStorage = new artjs.LocalStorage("todos-artjs");
+  this._listWatcher = new art.service.ListWatcher("todo-list", this);
+  this._model.onItemAdd.add(artjs.$D(this, "_onItemAdd"));
+  this._model.onItemChange.add(artjs.$D(this, "_onItemChange"));
+  artjs.Broadcaster.addListener("Filter", artjs.$D(this, "_onFilter"));
+  this._handle("Todo::New", "_onNew");
+  this._handle("MarkAllComplete", "_onMarkAllComplete");
+  this._handle("ClearCompleted", "_onClearCompleted");
+  this._handleEmit("Item::Delete", "_onDelete");
+  var items = this._localStorage.getItem("items");
+  this.setItems(art.model.Item.fromArray(!items || artjs.Array.isEmpty(items) ? art.service.db.items : items));
+}, {
+  _onNew: function(text) {
+    var value = text.getValue();
+    this.addItem(new art.model.Item(value));
+  },
+  _onDelete: function(item) {
+    this.removeItem(item.getModel());
+  },
+  update: function() {
+    var items = this._model.items;
+    if (!artjs.Object.isNull(items)) {
+      items = artjs.Array.invoke(items, "toJson");
+    }
+    this._localStorage.setItem("items", items);
+  },
+  _onMarkAllComplete: function(checkbox) {
+    this._allComplete = checkbox.isChecked();
+    artjs.Array.each(this._model.items, this._markAsComplete, this);
+  },
+  _onClearCompleted: function() {
+    this.removeItems(artjs.Array.select(this._model.items, this._isCompleted, this));
+  },
+  _markAsComplete: function(item) {
+    item.completed = this._allComplete;
+  },
+  _isCompleted: function(item) {
+    return item.completed;
+  },
+  _isActive: function(item) {
+    return !item.completed;
+  },
+  _true: function() {
+    return true;
+  },
+  _onItemChange: function(model, item, property) {
+    if (property == "completed") {
+      this._setItemVisibility(item);
+    }
+  },
+  _onItemAdd: function(model, item) {
+    this._setItemVisibility(item);
+  },
+  _onFilter: function(id) {
+    this._visibilityStrategy = this.ctor.VISIBILITY_STRATEGIES[id] || "_true";
+    this._filter();
+  },
+  _filter: function() {
+    artjs.Array.each(this._model.items, this._setItemVisibility, this);
+  },
+  _setItemVisibility: function(item) {
+    item.visible = this[this._visibilityStrategy](item);
+  }
+}, {
+  _name: "art.view.List",
+  VISIBILITY_STRATEGIES: {
+    completed: "_isCompleted",
+    active: "_isActive"
+  }
+}, artjs.ListView);
+
+art.view.MarkAllComplete = artjs.Class(function(element) {
+  this.super(element);
+  this._listWatcher = new art.service.ListWatcher("todo-list", this);
+}, {
+  update: function(listModel) {
+    var pluck = artjs.Array.pluck(listModel.items, "completed");
+    this.setChecked(artjs.Array.all(pluck));
+  }
+}, {
+  _name: "art.view.MarkAllComplete"
+}, artjs.CheckBox);
+
+art.view.Main = artjs.Class(function(element) {
+  this.super(element);
+  this._listWatcher = new art.service.ListWatcher("todo-list", this);
+}, {
+  update: function(listModel) {
+    artjs.Element.setVisible(this._element, artjs.Array.isNotEmpty(listModel.items));
+  }
+}, {
+  _name: "art.view.Main"
+}, artjs.View);
+
+art.view.New = artjs.Class(function(element) {
+  this.super(element);
+  artjs.on("keydown", element, artjs.$D(this, "_onEnter"), 13);
+}, {
+  _onEnter: function() {
+    var value = artjs.String.trim(this.getValue());
+    if (!artjs.String.isEmpty(value)) {
+      this._fire("Todo::New");
+      this.clear();
+    }
+  }
+}, {
+  _name: "art.view.New"
+}, artjs.Text);
